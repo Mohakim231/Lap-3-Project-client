5058fa18b892f82d1f3a36df236d5d92c2dd967d
Sadik Islam <sadikislam46@gmail.com>
2023-03-07 15:53:59 +0000

    exported secureRoute

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as secureRoute } from './secureRoute';
 export { default as NavBar } from './NavBar';
 export { default as Post } from './Post';
 export { default as Note } from './Note';
