f530d748bdd933cb42cc305f13fa0936e6462528
Sadik Islam <sadikislam46@gmail.com>
2023-03-07 15:50:05 +0000

    secureRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import NavBar from './components/NavBar'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import secureRoute from './components/secureRoute';
 import { Create, Forum, Home, PublicNote, Signup, Login } from './pages'
 
 function App() {
