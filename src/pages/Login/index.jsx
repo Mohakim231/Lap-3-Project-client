39403dc5fddc4014644f7b51fb790464606e4ff3
Sadik Islam <sadikislam46@gmail.com>
2023-03-07 11:53:00 +0000

    implemented axios in login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,90 +1,38 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import anxios from "axios";
+import axios from 'axios';
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleUsername = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Email: ${email}, Password: "{password}`)
-  };
-  
-  return (
-    <div>
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-        <div>
-            <label type="username" value={username} onChange={handleUsername} />
-        </div>
-        <div>
-            <label type="password" value={password} onChange={handlePassword} />
-        </div>
-        <button type="submit">Submit</button>
-        </form>
-    </div>
-  );
-};
-
-const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleName = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleEmail = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/Login", {
+        user,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <div>
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name</label>
-          <input type="text" value={name} onChange={handleName} />
-        </div>
-        <div>
-          <label>Email</label>
-          <input type="email" value={email} onChange={handleEmail} />
-        </div>
-        <div>
-          <h2>Password</h2>
-          <input type="password" value={password} onChange={handlePassword} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <div>
+        <label>Username</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.targetvalue)} />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button type='submit'>Login</button>
+    </form>
   );
 };
 
-const App = () => {
-    return (
-        <div>
-            <Login />
-            <Signup />
-        </div>
-    );
-};
-
-export default App;
+export default Login;
