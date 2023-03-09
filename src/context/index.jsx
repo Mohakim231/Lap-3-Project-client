0a015c8d5ff14bdd32e9c95e9792ce3814a9a34f
shojib deb <shodeb123@gmail.com>
2023-03-09 16:37:26 +0000

    user_name added to context

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,12 +4,14 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(0);
+    const [user_name, setUser_name] = useState("")
 
     return (
-        <AuthContext.Provider value={{ user, setUser }}>
+        <AuthContext.Provider value={{ user, setUser, user_name, setUser_name }}>
             {children}
         </AuthContext.Provider>
     );
+
 };
 
 export const useAuth = () => useContext(AuthContext);
--- src/pages/Forum/index.jsx
+++ src/pages/Forum/index.jsx
@@ -29,18 +29,23 @@ const Forum = () => {
       clearCategory();
       handleCategoryClick(c.note_category);
     }} key={c.note_category}>{c.note_category}</div>)
-    // notes.map(n => <Note key={n.note_id} id={n.note_id} title={n.note_title} content={n.note_content} category={n.note_category}/>)
+
   }
 
 
 
   return (
-    <div aria-label='note'>
-      <div>
-        {showCategory()}
+    <>
+      <div aria-label='note'>
+        <div>
+          {showCategory()}
+        </div>
+        {categoryId && <PublicNote id={categoryId} />}
       </div>
-      {categoryId && <PublicNote id={categoryId} />}
-    </div>
+      <div>
+        <Comment>asdas</Comment>
+      </div>
+    </>
   )
 }
 
